f8f574c0f4107ed8970a2bd8b7d5ca9b3a4b72cc
chen <chenhackmon@gmail.com>
2024-07-04 13:57:54 +0300

    project chen hackmon

---
+++ yourSolution.sh
@@ -0,0 +1,15 @@
+mkdir secretDir
+rm maliciousFiles
+rmdir  maliciousFiles/
+rm someFileIsLinkingToMe.BeAware
+rm amIMaliciousOrNot.whoKnows
+cd ..
+rmdir  maliciousFiles/
+cd secretDir/
+touch .secret
+sudo chmod 600 .secret
+/bin/bash generateSecret.sh
+rm important.link
+bin/bash generateSecret.sh
+cd secretDir/
+cat .secret
